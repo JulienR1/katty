c3737af7a6e575e2393c9a177383c0daa5e710d4
Julien Rousseau <julrousseau20@gmail.com>
2021-09-15 23:47:55 -0400

    Listening to discord channel

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log("Hello world!");
+import dotenv from "dotenv";
+import { Client, Intents } from "discord.js";
+
+dotenv.config();
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+
+client.on("ready", () => {
+	console.log(`Logged in as ${client.user?.tag}!`);
+});
+
+client.on("messageCreate", (message) => {
+	console.log(message);
+});
+
+client.login(process.env.DISCORD_TOKEN);
