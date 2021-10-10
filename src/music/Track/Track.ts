9dbf3f84bb257f1c07de9642d3982e560177d699
Julien Rousseau <julrousseau20@gmail.com>
2021-10-10 15:37:58 -0400

    Cleaned track object

--- src/music/Track/Track.ts
+++ src/music/Track/Track.ts
@@ -1,96 +1,53 @@
 import { AudioResource, createAudioResource, demuxProbe } from "@discordjs/voice";
-import { raw } from "youtube-dl-exec";
-import ytSearch from "yt-search";
-import ytdl from "ytdl-core";
-import { YoutubeTrackFactory } from "./Factories/YoutubeTrackFactory";
+import { Options, ExecaChildProcess } from "execa";
 import { ITrack, ITrackData } from "./ITrack";
+import { raw } from "youtube-dl-exec";
+import { Readable } from "stream";
 
 export class Track implements ITrack {
-	// TODO: remove variables
-	public title: string | undefined = undefined;
-	public url: string | undefined = undefined;
-	public thumbnailURL: string | undefined = undefined;
+	private downloadFlags = { o: "-", q: "", f: "bestaudio[ext=webm+acodec=opus+asr=48000]/bestaudio", r: "100K" };
+	private downloadOptions: Options<string> = { stdio: ["ignore", "pipe", "ignore"] };
 
-	constructor(private trackData: ITrackData) {}
+	constructor(private trackData: ITrackData) {
+		if (parseInt(this.trackData.lengthSeconds) >= 3600) {
+			throw new Error("Cannot play a song longer than 60 minutes.");
+		}
+	}
 
 	public getData(): ITrackData {
 		return JSON.parse(JSON.stringify(this.trackData));
 	}
 
-	// TODO
-	public getAudioResource(): Promise<AudioResource<Track>> {
-		return undefined as any;
-	}
-
-	public getStream(): Promise<AudioResource<Track>> {
-		if (!this.url) {
+	public async getAudioResource(): Promise<AudioResource<Track>> {
+		if (!this.trackData.url) {
 			throw new Error("Invalid track.");
 		}
 
-		const videoUrl = this.url;
+		const process = raw(this.trackData.url, this.downloadFlags, this.downloadOptions);
+		if (!process.stdout) {
+			throw new Error("No output stream from track.");
+		}
+
+		return this.fetchTrackResource(process, process.stdout);
+	}
+
+	private fetchTrackResource(process: ExecaChildProcess<string>, stream: Readable): Promise<AudioResource<Track>> {
 		return new Promise((resolve, reject) => {
-			const process = raw(
-				videoUrl,
-				{ o: "-", q: "", f: "bestaudio[ext=webm+acodec=opus+asr=48000]/bestaudio", r: "100K" },
-				{ stdio: ["ignore", "pipe", "ignore"] }
-			);
-
-			if (!process.stdout) {
-				return reject(new Error("No output stream"));
-			}
-
-			const stream = process.stdout;
 			const onError = (err: Error) => {
 				if (!process.killed) {
 					process.kill();
 				}
 				stream.resume();
-				reject(err);
+				return reject(err);
 			};
 
-			process
-				.once("spawn", async () => {
-					try {
-						const probe = await demuxProbe(stream);
-						return resolve(createAudioResource(probe.stream, { metadata: this, inputType: probe.type }));
-					} catch (err) {
-						onError(err as Error);
-					}
-				})
-				.catch(onError);
+			const onSpawn = () => {
+				demuxProbe(stream)
+					.then((probe) => resolve(createAudioResource(probe.stream, { metadata: this, inputType: probe.type })))
+					.catch(onError);
+			};
+
+			process.once("spawn", onSpawn).catch(onError);
 		});
-
-		// return ytdl(this.url, { filter: "audioonly" });
-	}
-
-	public async fromURL(typedUrl: string): Promise<Track> {
-		new YoutubeTrackFactory().from(typedUrl);
-		const youtubeRegex = /^(https?\:\/\/)?(www\.youtube\.com|youtu\.?be)\/.+$/;
-
-		if (typedUrl.match(youtubeRegex)) {
-			const trackInfo = await ytdl.getInfo(typedUrl);
-
-			this.title = trackInfo.videoDetails.title;
-			this.url = trackInfo.videoDetails.video_url;
-			this.thumbnailURL = trackInfo.videoDetails.thumbnails[1].url;
-
-			return this;
-		}
-		throw new Error(`Invalid or unsupported music URL: ${typedUrl}`);
-	}
-
-	public async fromKeywords(keywords: string[]): Promise<Track> {
-		const args = keywords.join(" ");
-		const result = await ytSearch(args);
-
-		if (result.videos.length > 0) {
-			const videoData = result.videos[0];
-			this.title = videoData.title;
-			this.url = videoData.url;
-			this.thumbnailURL = videoData.thumbnail;
-			return this;
-		}
-
-		throw new Error(`Could not find any video with the specified keywords: '${args}'`);
 	}
 }
