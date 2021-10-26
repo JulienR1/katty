2a6802a5dba1bfd831ab7b68faa3a956008d343f
Julien Rousseau <julrousseau20@gmail.com>
2021-10-25 22:15:20 -0400

    Shuffle command

--- src/commands/CommandHandler.ts
+++ src/commands/CommandHandler.ts
@@ -1,59 +1,52 @@
 import { ICommand } from "./models/ICommand";
 import { CommandVerb } from "./VerbRegistry";
 import { CommandRequest } from "./CommandRequest";
-import {
-	JoinCommand,
-	KickCommand,
-	NextCommand,
-	PlayCommand,
-	PauseCommand,
-	ResumeCommand,
-	QueueCommand,
-	ClearCommand,
-} from "./implementations";
+import { JoinCommand, KickCommand, NextCommand, PlayCommand, PauseCommand, ResumeCommand, QueueCommand, ClearCommand } from "./implementations";
 import { LoopCommand } from "./implementations/LoopCommand";
+import { ShuffleCommand } from "./implementations/ShuffleCommand";
 
 export class CommandHandler {
-	private static instance: CommandHandler | undefined = undefined;
-	private commands: { [key in CommandVerb]?: ICommand } = {};
+  private static instance: CommandHandler | undefined = undefined;
+  private commands: { [key in CommandVerb]?: ICommand } = {};
 
-	private constructor() {
-		const unmappedCommands: ICommand[] = [
-			new JoinCommand(),
-			new KickCommand(),
-			new PlayCommand(),
-			new NextCommand(),
-			new PauseCommand(),
-			new ResumeCommand(),
-			new QueueCommand(),
-			new LoopCommand(),
-			new ClearCommand(),
-		];
+  private constructor() {
+    const unmappedCommands: ICommand[] = [
+      new JoinCommand(),
+      new KickCommand(),
+      new PlayCommand(),
+      new NextCommand(),
+      new PauseCommand(),
+      new ResumeCommand(),
+      new QueueCommand(),
+      new LoopCommand(),
+      new ClearCommand(),
+      new ShuffleCommand(),
+    ];
 
-		unmappedCommands.reduce((obj, command) => {
-			obj[command.getVerb()] = command;
-			return obj;
-		}, this.commands);
-	}
+    unmappedCommands.reduce((obj, command) => {
+      obj[command.getVerb()] = command;
+      return obj;
+    }, this.commands);
+  }
 
-	public static Instance(): CommandHandler {
-		if (CommandHandler.instance === undefined) {
-			CommandHandler.instance = new CommandHandler();
-		}
-		return CommandHandler.instance;
-	}
+  public static Instance(): CommandHandler {
+    if (CommandHandler.instance === undefined) {
+      CommandHandler.instance = new CommandHandler();
+    }
+    return CommandHandler.instance;
+  }
 
-	public executeCommand(request: CommandRequest) {
-		const command = this.commands[request.verb];
-		if (command && request.description) {
-			try {
-				command.execute(request.description);
-			} catch (err) {
-				console.error(err);
-			}
-		} else {
-			console.error("The specified command does not exist.");
-			throw new Error("The specified command does not exist.");
-		}
-	}
+  public executeCommand(request: CommandRequest) {
+    const command = this.commands[request.verb];
+    if (command && request.description) {
+      try {
+        command.execute(request.description);
+      } catch (err) {
+        console.error(err);
+      }
+    } else {
+      console.error("The specified command does not exist.");
+      throw new Error("The specified command does not exist.");
+    }
+  }
 }
